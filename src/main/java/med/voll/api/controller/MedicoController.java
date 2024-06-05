71aeda1ff9f961575f80d61958a54b2cc39ca61d
giovaniocan <giovaniocan@gmail.com>
2024-06-05 08:03:44 -0300

    made new things

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,31 +20,35 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) { // esse Pageable serve para paginar o que estamos buscando
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) { // esse Pageable serve para paginar o que estamos buscando
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
 
+        return ResponseEntity.ok(page);
         // sem a paginacao fica assim return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
     @PutMapping
     @Transactional// precisa quando vai fazer algo (inserir) no bd
-    public void atualizar(@RequestBody @Valid DadosatualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosatualizacaoMedico dados){
         var medico = repository.getReferenceById(dados.id());
 
         medico.atualizarInfo(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }// nao precisa chamar o repository no final, quando a gente ja muda as coisas ele reflete automaticamente no banco de dados, pois tem a tag @Transctional
 
     @Transactional
     @DeleteMapping("/{id}")
-    public void excluir(@PathVariable Long id){
+    public ResponseEntity excluir(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
     }
 
 }
