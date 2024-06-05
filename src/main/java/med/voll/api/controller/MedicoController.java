67958cf60d77553bd258697097b60efd41bc662e
giovaniocan <giovaniocan@gmail.com>
2024-06-05 08:36:07 -0300

    developed the right methodo do create

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("medicos")
@@ -20,8 +21,13 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dados);
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping
@@ -29,7 +35,6 @@ public class MedicoController {
         var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
 
         return ResponseEntity.ok(page);
-        // sem a paginacao fica assim return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
     @PutMapping
@@ -50,5 +55,6 @@ public class MedicoController {
 
         return ResponseEntity.noContent().build();
     }
+
 
 }
