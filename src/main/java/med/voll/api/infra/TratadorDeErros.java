c33858613a870b36e2a44f89f21ffaf9c38317d4
giovaniocan <giovaniocan@gmail.com>
2024-07-16 20:58:12 -0300

    created error treatment for bad request (400)

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,4 +16,19 @@ public class TratadorDeErros {
         return ResponseEntity.notFound().build();
     }
 
+    // anotacao para quando os argumentos por algum motivo nao esta de acordo com o esperado
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException ex){ // estamos recebendo a excecao
+        var erros = ex.getFieldErrors();
+
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacao::new).toList());
+    }
+
+    // aqui é um record de como ficaria os dados do resposta desse erro, apenas qual o campo que ta com erro e qual a mensagem de erro por si so
+    private record DadosErrosValidacao(String campo, String mensagem){
+        public DadosErrosValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
 }
