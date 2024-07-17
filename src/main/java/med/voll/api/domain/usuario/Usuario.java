aeaec958a04d1e3f7ec6f98990e5ef56793bee99
giovaniocan <giovaniocan@gmail.com>
2024-07-17 19:16:51 -0300

    created users jpa class, migrations, and repository

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//todas as anotacoes necesarias para poder criar a jpa
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
