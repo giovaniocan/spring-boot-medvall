d83d537ffd22e9a67b5b7d0a3139643dd3dbe127
giovaniocan <giovaniocan@gmail.com>
2024-07-23 20:46:30 -0300

    crated the basics validation to add an apointment

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,8 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,14 +12,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// o controler apenas mapea os end points, faz as validacoes do bean validation,
+// pega os dados e manda para a service, que vai fazer as validacoes
+
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultas agenda;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
-        System.out.println(dados);
+        agenda.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 
